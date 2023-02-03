0cda41f69e7f6401f31ceb7fac328a16bee6f9c7
Fernando <fernandomercado529@gmail.com>
2023-02-03 00:28:58 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:app_shoes/screens/shoes_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Shoes App',
+        debugShowCheckedModeBanner: false,
+        home: ShoesScreen());
+  }
+}
